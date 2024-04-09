82d31f90d6f2c8eb1de2023a88aaea1c23327bc3
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-04-09 17:19:18 +0530

    UI Styling Changes - SignUp Page

--- web-client/src/components/SignUpFlow.jsx
+++ web-client/src/components/SignUpFlow.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 
+
 const SignUpFlow = () => {
 
 
@@ -30,11 +31,103 @@ function handleFormSubmit(ev){
 
 
   return (
-    <div>
-        <div>Image Div</div>
-        <div>
-            <p>Already a member? Sign In</p>
-            <h2>Sign up to Dribble</h2>
+    <div className='flex bg-slate-200 justify-center'>
+        <div className='hidden md:inline sm:w-2/5'>Image Div</div>
+        
+        <div className='sm:w-3/5 bg-red-500'>
+            <div className='text-right'>
+                <span>Already a member?</span>
+                <span className='font-bold text-[#473B8F]'>Sign in</span>
+            </div>
+
+
+
+<div className='bg-red-200 sm:max-w-[380px] max-w-md mx-auto'>
+            <div className='mb-5'>
+                <span className='text-3xl font-semibold'>Sign up to Dribble</span>
+            </div>
+
+            <div className='mb-6'>
+                <span>
+                    <ul role='list' className='marker:text-red-500 list-disc pl-5 space-y-3 text-red-500'>
+                        <li role='listitem'>
+                            Username is already been taken
+                        </li>
+                    </ul>
+                </span>
+            </div>
+
+
+            <form>
+
+<div className='flex  justify-between flex-wrap mb-6 '>
+
+<div className='mb-6'>
+<div>
+    <span>Icon</span>
+    <label>Name</label>
+</div>
+<div>
+<input type='text'></input>
+</div>
+</div>
+
+
+<div>
+<div>
+    <span>Icon</span>
+    <label>Name</label>
+</div>
+<div>
+<input type='text'></input>
+</div>
+</div>
+
+</div>
+
+
+<div className='mb-6'>
+    
+<div>
+    <span>Icon</span>
+    <label>Name</label>
+</div>
+<div>
+<input type='text' className='w-full'></input>
+</div>
+
+
+
+</div>
+
+<div className='mb-6'>
+    
+<div>
+    <span>Icon</span>
+    <label>Name</label>
+</div>
+<div>
+<input type='text' className='w-full'></input>
+</div>
+
+
+
+</div>
+
+</form>
+
+<div className='mb-6 flex gap-1 items-start'>
+    <input id='verify-terms' type='checkbox' />
+    <label htmlFor='verify-terms'>Creating an account means you're okay with out terms of Service, Privacy Policy, and out default Notification Settings</label>
+</div>
+
+<button className="bg-[#EA4B8B] hover:bg-[#ea4b8ba3] text-white font-bold py-2 px-8 rounded-lg">Create Account</button>
+</div>
+
+
+
+
+            {/* <h2>Sign up to Dribble</h2>
             {error && <ul><li>Username has already been taken</li></ul>}
             <form onSubmit={handleFormSubmit}>
 
@@ -46,9 +139,10 @@ function handleFormSubmit(ev){
                 <input type='email' id='email' onChange={(ev)=> setEmail(ev.target.value)} />
                 <label htmlFor='password'>Password</label>
                 <input type='password' id='password' onChange={(ev)=> setPassword(ev.target.value)} />
-                <button>Create Account</button>
+                <button className="bg-[#EA4B8B] hover:bg-[#ea4b8ba3] text-white font-bold py-2 px-8 rounded-lg">Create Account</button>
 
-            </form>
+            </form> */}
+
         </div>
     </div>
   )
