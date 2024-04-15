4e592d8652e668995dbf233be312902c94e6dfdf
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-04-15 17:51:58 +0530

    using a custom error handling middleware solved the previous issue, but still if any error occurs in this middleware, then the response is sent back to the client, so we need to stay cautious here

--- api/index.js
+++ api/index.js
@@ -32,3 +32,13 @@ app.use('/api', userRouter)
 
 
 //error handler
+//error handler
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal server error"
+    res.status(statusCode).json({
+        "success": "false",
+        message,
+        statusCode
+    })
+})
