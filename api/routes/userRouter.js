8c59cf8513a1f8c68cf2decb3ed5d9711edd4048
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-04-16 14:34:13 +0530

    User creation after successful validation - done. Next step to hash password before creating user

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -7,6 +7,8 @@ const router = express.Router()
 //validate and create new user
 router.post('/users', async (req,res,next)=> {
     try {
+
+        //if there is no required data in request body, we will return error response from here
         if (!req.body.name, !req.body.username, !req.body.email, !req.body.password) {
             //using 422 for Unprocessable Entity
             return res.status(422).json({
@@ -14,29 +16,37 @@ router.post('/users', async (req,res,next)=> {
                 "message": "Required fields are missing",
                 "responseData": null
             })
-
-            
-            
         }  
         
+        //if there is required data in request body
         if (req.body.name, req.body.username, req.body.email, req.body.password) {
-            const {name, username, email, password} = req.body
-
             
+            
+            const {name, username, email, password} = req.body
             try {
-                
-                //validating request body and storing it in value if validation is ok
-                const value = await userValidationSchema.validateAsync({name, username,email, password});
-                console.log(value)
+                //validating request body using Joi and storing it in validatedValue variable if validation is ok
+                const validatedValue = await userValidationSchema.validateAsync({name, username,email, password});
                 //if validation is success, we will further check the username 
-                if (value){
-                    const checkUsername = await USER.findOne({username: value.username})
-                console.log("username found is",checkUsername)
-                if (!checkUsername) {
-                    console.log("saving username")
-                    const newCreatedUser = await USER.create(value)
-                    console.log(newCreatedUser)
-                } else return res.status(401).json({"msg": "hii"})
+                if (validatedValue){
+                    const checkUsername = await USER.findOne({username: validatedValue.username})
+                    const checkEmail = await USER.findOne({email: validatedValue.email})
+
+                    console.log(checkUsername, "check username")
+                    console.log(checkEmail, "check email")
+                
+                    //if the username is found, we will further save the user else we will return response from here that username exists
+                if (!checkUsername && !checkEmail) {
+                    const newCreatedUser = await USER.create(validatedValue)
+                    if (newCreatedUser) {
+                        return res.status(201).json({success:true, message: "User created successfully", responseData: newCreatedUser})
+                    }
+                } else 
+                if (!checkEmail) {
+                    return res.status(409).json({success:false, message: "Username already exists", responseData: null })
+
+                } else
+
+                return res.status(409).json({success:false, message: "Email already exists", responseData: null })
 
 
 
