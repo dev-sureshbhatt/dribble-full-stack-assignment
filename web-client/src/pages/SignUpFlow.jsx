4deb0d0ea6d7f700200cbddcdc5f2c56e60e9b05
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-04-16 18:12:00 +0530

    Form now send POST request on submit. The backend already can process this data.

--- web-client/src/pages/SignUpFlow.jsx
+++ web-client/src/pages/SignUpFlow.jsx
@@ -19,6 +19,17 @@ const SignUpFlow = () => {
     ev.preventDefault();
     // Send formData to API
     console.log(formData);
+
+    //sending fetch request
+    try {
+      fetch('http://localhost:4000/api/users/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData)
+      })
+    } catch (error) {
+      console.log(error)
+    }
   };
     
   
